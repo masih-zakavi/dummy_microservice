28c11ae6add4f68b8060eadb20cfdc62fcaa5134
masih-zakavi <123717754+masih-zakavi@users.noreply.github.com>
2023-12-16 20:36:11 -0500

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -22,7 +22,7 @@ async def helloWorld():
 
 if __name__ == "__main__":
     uvicorn.run("HelloWorld:app",
-                host="127.0.0.1",
+                host="0.0.0.0",
                 port=8000,
                 reload=True,
                 log_level="debug")
